12b0496b0491b791e009dca8472a30f406ca2ab8
dhritishar <109621401+dhritishar@users.noreply.github.com>
2022-09-18 20:39:34 -0400

    added user prompt for password length

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//define function generatePassword
+function generatePassword () {
+  //createe value for generatePassword
+  //prompt when you click the red generate password button
+  var input = window.prompt ('How long do you want your password to be?')
+  //determnine password length using a number input
+  var pswdLength = parseInt(input)
+//parseInt will determine if it is a number, following statements show what the response will be for a true number or not
+  if (isNaN(pswdLength)) {
+    window.alert('Please input a number!')
+  } else {
+    window.alert('Thank you, this is a valid number.')
+  }
+    
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
