ef7265f032467484b468e2096fd323487d82535a
dhritishar <109621401+dhritishar@users.noreply.github.com>
2022-09-18 20:57:50 -0400

    added validation for user wanting to add numbers, upper and lower case letters and symbols

--- script.js
+++ script.js
@@ -3,18 +3,39 @@ var generateBtn = document.querySelector("#generate");
 
 //define function generatePassword
 function generatePassword () {
-  //createe value for generatePassword
-  //prompt when you click the red generate password button
+
+//createe value for generatePassword
+//prompt when you click the red generate password button
   var input = window.prompt ('How long do you want your password to be?')
-  //determnine password length using a number input
+
+//determnine password length using a number input
   var pswdLength = parseInt(input)
+
 //parseInt will determine if it is a number, following statements show what the response will be for a true number or not
   if (isNaN(pswdLength)) {
-    window.alert('Please input a number!')
+    window.alert('Oops! That is not a number, please input a number!')
   } else {
     window.alert('Thank you, this is a valid number.')
   }
-    
+//need to determine if password length is withing 8-128 character condition
+  if (pswdLength <8 || pswdLength >128) {
+    window.alert ('Reminder! Password length must be between 8 and 128 characters!')
+    return
+  }
+ 
+//confirm if user wants to use numbers in the password
+var userConfirmNumbers = window.confirm("Would you like to use numbers in your password?")
+
+//confirm if user wants to use lower case or upper case in the password
+var userConfirmLowercase = window.confirm('Would you like to use lowercase letters in your password?')
+var userConfirmUppercase = window.confirm('Would you like to use uppercase letters in your password?')
+
+//confirm if user wants to use symbols in the password
+var userConfirmSymbols = window.confirm('Would you like to use symbols in your password?')
+
+
+
+
 }
 
 // Write password to the #password input
